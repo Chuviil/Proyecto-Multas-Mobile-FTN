ca59bdd7b684def60dfbde0f27429ae87ed3c066
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-03 17:27:32 -0500

    created basic constants for COLORS, SIZES, URL

---
+++ constants/theme.js
@@ -0,0 +1,20 @@
+const COLORS = {
+    primary: "#FFBB38",
+    lightColor: "#F5F5F5",
+    darkColor: "#111111",
+};
+
+const FONT = {
+
+};
+
+const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+};
+
+export { COLORS, FONT, SIZES };
