8bdef1bf25ed176ace12ed1b9c3dfc868530a3f6
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-05 23:29:04 -0500

    added udla logo in both variants: white, black

--- constants/images.js
+++ constants/images.js
@@ -1,7 +1,9 @@
 import main_bg from "../assets/images/hero-bg.jpg"
 import udla_logo_blanco from "../assets/images/udla_logo_blanco.png"
+import udla_logo_negro from "../assets/images/udla_logo_negro.png"
 
 export default {
     main_bg,
-    udla_logo_blanco
+    udla_logo_blanco,
+    udla_logo_negro
 }
