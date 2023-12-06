9bd161fc5d36a66748923b3795c690c294b6684d
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-05 23:29:38 -0500

    created /multas page and modified login to redirect to it

--- app/login.js
+++ app/login.js
@@ -1,5 +1,5 @@
 import {Image, SafeAreaView, Text, View} from "react-native";
-import {Stack} from "expo-router";
+import {router, Stack} from "expo-router";
 import {COLORS, images} from "../constants";
 import {StatusBar} from "expo-status-bar";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
@@ -17,13 +17,22 @@ const Login = () => {
             <View style={{flex: 1, alignItems: 'center', backgroundColor: COLORS.darkColor, paddingTop: insets.top}}>
                 <Image resizeMode={"contain"} source={images.udla_logo_blanco}
                        style={{width: 322, height: 116, marginTop: 45}}/>
-                <View style={{width: '80%'}}>
-                    <Text style={{fontSize: 60, color: COLORS.lightColor, fontWeight: 'bold', textAlign: 'center'}}>Login</Text>
+                <View style={{width: '80%', marginTop: 60}}>
+                    <Text style={{
+                        fontSize: 45,
+                        color: COLORS.lightColor,
+                        fontWeight: 'bold',
+                        textAlign: 'center'
+                    }}>Login</Text>
+                    <Text style={{color: COLORS.lightColor, textAlign: 'center', marginBottom: 60}}>Revisemos tus
+                        multas!</Text>
                     <TextField label={"Id Banner"}/>
                     <TextField label={"Contraseña"} password/>
-                    <MaterialButton>
-                        Iniciar Sesión
-                    </MaterialButton>
+                    <View style={{marginTop: 60}}>
+                        <MaterialButton onPress={() => router.replace("/multas")}>
+                            Iniciar Sesión
+                        </MaterialButton>
+                    </View>
                 </View>
             </View>
             <StatusBar style={"light"}/>
