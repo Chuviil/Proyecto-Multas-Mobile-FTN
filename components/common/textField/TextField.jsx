cc6ce242a5ac3a6de77b0e5cf4d0e978b4f821e1
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-13 09:32:29 -0500

    modified textField component to have dark or white text

--- components/common/textField/TextField.jsx
+++ components/common/textField/TextField.jsx
@@ -1,11 +1,11 @@
 import {Text, TextInput, View} from "react-native";
 import styles from "./textField.style";
 
-const TextField = ({label, password, labelColor, onChangeText}) => {
+const TextField = ({label, password, labelColor, onChangeText, dark}) => {
     return (
         <View style={styles.textFieldContainer}>
             <Text style={styles.labelText(labelColor)}>{label}</Text>
-            <TextInput onChangeText={onChangeText} secureTextEntry={password} style={styles.textInput}/>
+            <TextInput onChangeText={onChangeText} secureTextEntry={password} style={styles.textInput(dark)}/>
         </View>
     )
 }
